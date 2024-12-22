0686ee9fb9b576995ab852ed0ab5f1ede7cdfbfb
FAST <krinickij.v.a@gmail.com>
2024-12-22 12:02:49 +0300

    CHORE: aded libs to project
    Добавлено:
    vue-router
    pinia
    vee-validate/yup
    vee-validate
    tailwindCss
    
    Удалены неиспользуемые файлы из базового проекта vite

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import router from './router'
+const pinia = createPinia();
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+    .use(router)
+    .use(pinia)
+    .mount('#app')
