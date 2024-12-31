a83c92fdd74ccf93ebd2aadd354db2ee7c69e60b
krinickij <krinickij.v.a@gmail.com>
2024-12-30 23:11:37 -0500

    FEAT: edited  lessons preview

--- src/pages/LessonsListPage.vue
+++ src/pages/LessonsListPage.vue
@@ -12,23 +12,21 @@ import LessonPreview from '../components/lessons/LessonPreview.vue';
             nav="home"
             createPage="createLesson"/>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
+            <!-- <LessonPreview
+            title="title1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil beatae ratione adipisci? At molestias culpa possimus aspernatur qui in distinctio."/> -->
             <LessonPreview
-            title="title1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil beatae ratione adipisci? At molestias culpa possimus aspernatur qui in distinctio."/> 
-            
+            imgUrl="https://avatars.mds.yandex.net/i?id=7c294aa45665ea7566ae2e4e3960f943_l-5129576-images-thumbs&n=13"
+            desc="
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur error possimus exercitationem deleniti maiores. Asperiores reiciendis pariatur rerum, quis officiis repellat repellendus iusto, veniam dolore libero, consectetur corporis vero doloribus! Deserunt nesciunt odio voluptatum animi est repellendus commodi culpa, consectetur voluptas reprehenderit perferendis voluptate quia molestiae exercitationem, perspiciatis quis illum?"
+            title="title2"
+            :isPubllished="false"/>
             <LessonPreview
-            title="title2"/>
-            <LessonPreview
-            title="title3"/>
-            <LessonPreview
-            title="title4"/>
-            <LessonPreview
-            title="title5"/>
-            <LessonPreview
-            title="title6"/>
-            <LessonPreview
-            title="title7"/>
-            <LessonPreview
-            title="title8"/>
+
+            desc="
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur error possimus exercitationem deleniti maiores. Asperiores reiciendis pariatur rerum, quis officiis repellat repellendus iusto, veniam dolore libero, consectetur corporis vero doloribus! Deserunt nesciunt odio voluptatum animi est repellendus commodi culpa, consectetur voluptas reprehenderit perferendis voluptate quia molestiae exercitationem, perspiciatis quis illum?"
+            title="title2"
+            :isPubllished="false"/>
+
         </div>
         <Pagination/>
     </MainLauout>
