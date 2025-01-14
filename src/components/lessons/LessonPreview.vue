ec44b43ba329c815f7e08945b89813287226f872
Vitalij <krinickij.v.a@gmail.com>
2025-01-14 15:01:47 +0300

    FEAT: edited lessons preview lessons

--- src/components/lessons/LessonPreview.vue
+++ src/components/lessons/LessonPreview.vue
@@ -1,38 +1,41 @@
 <template>
 <div class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm  flex flex-col gap-5 relative">
-    <div v-if="isPubllished" class="absolute top-0 right-0 bg-green-600 h-20 w-20 rounded-bl-full flex items-center " >
+    <div v-if="post.isPubllished" class="absolute top-0 right-0 bg-green-600 h-20 w-20 rounded-bl-full flex items-center " >
         <span class="  rotate-45  text-lg font-bold text-white ">published</span>
 
     </div>
 
     <div class="flex flex-col gap-2">
         <div class=" h-40 w-full">
-            <img class=" w-full h-full object-contain" :src="`${imgUrl? imgUrl : '/images/noimage.png'}`" alt="">
+            <img class=" w-full h-full object-contain" :src="`${post.imgUrl? post.imgUrl : '/images/noimage.png'}`" alt="">
 
         </div>
 
-        <h2 class=" text-xl font-semibold tracking-tight text-gray-900 min-w-full whitespace-nowrap overflow-ellipsis overflow-hidden">{{ title }}</h2>
-        <p >{{ desc }}</p>
+        <h2 class=" text-xl font-semibold tracking-tight text-gray-900 min-w-full whitespace-nowrap overflow-ellipsis overflow-hidden">{{ post.title }}</h2>
+        <p >{{ post.desc }}</p>
     </div>
         <div class="grid grid-cols-2 gap-3">
-            <button class=" flex flex-row gap-4 items-center justify-center text-xl font-bold focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300  me-2   rounded-lg  px-5 py-2.5 ">
-                Редактировать
-            </button>
-            <EditButton/>
-            <button class=" focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 rounded-lg text-xl px-5 py-2.5 me-2  font-bold">Удалить</button>
+            <DeleteButton
+                addClass="w-full"/>
+            <EditButton
+                class="w-full"/>
         </div>
       </div>
 </template>
 
 <script setup lang="ts">
+import DeleteButton from '../ui/DeleteButton.vue';
 import EditButton from '../ui/EditButton.vue';
-import Icons from '../ui/Icons.vue';
 
-interface Props {
-    title:String,
-    isPubllished?: Boolean
-    desc: String,
+
+interface Data {
+    title:string,
+    isPubllished?: boolean
+    desc: string,
     imgUrl?:string
+}
+interface Props{
+    post:Data
 }
     defineProps<Props>()
 </script>
--- src/pages/LessonsListPage.vue
+++ src/pages/LessonsListPage.vue
@@ -3,6 +3,22 @@ import Pagination from '../components/pagination/Pagination.vue';
 import SubHeader from '../components/general/SubHeader.vue';
 import MainLauout from '../components/lauouts/MainLauout.vue';
 import LessonPreview from '../components/lessons/LessonPreview.vue';
+const data = [
+    {
+        'id':'11',
+        'title':'title1',
+        'imgUrl':'https://avatars.mds.yandex.net/i?id=7c294aa45665ea7566ae2e4e3960f943_l-5129576-images-thumbs&n=13',
+        'isPubllished':false,
+        'desc':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur error possimus exercitationem deleniti maiores. Asperiores reiciendis pariatur rerum, quis officiis repellat repellendus iusto, veniam dolore libero, consectetur corporis vero doloribus! Deserunt nesciunt odio voluptatum animi est repellendus commodi culpa, consectetur voluptas reprehenderit perferendis voluptate quia molestiae exercitationem, perspiciatis quis illum?"'
+    },
+    {
+        'id':'123',
+        'title':'title12',
+        'imgUrl':'',
+        'isPubllished':true,
+        'desc':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur error possimus exercitationem deleniti maiores. Asperiores reiciendis pariatur rerum, quis officiis repellat repellendus iusto, veniam dolore libero, consectetur corporis vero doloribus! Deserunt nesciunt odio voluptatum animi est repellendus commodi culpa, consectetur voluptas reprehenderit perferendis voluptate quia molestiae exercitationem, perspiciatis quis illum?"'
+    }
+]
 </script>
 
 <template>
@@ -12,20 +28,9 @@ import LessonPreview from '../components/lessons/LessonPreview.vue';
             nav="home"
             createPage="createLesson"/>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-            <!-- <LessonPreview
-            title="title1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil beatae ratione adipisci? At molestias culpa possimus aspernatur qui in distinctio."/> -->
-            <LessonPreview
-            imgUrl="https://avatars.mds.yandex.net/i?id=7c294aa45665ea7566ae2e4e3960f943_l-5129576-images-thumbs&n=13"
-            desc="
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur error possimus exercitationem deleniti maiores. Asperiores reiciendis pariatur rerum, quis officiis repellat repellendus iusto, veniam dolore libero, consectetur corporis vero doloribus! Deserunt nesciunt odio voluptatum animi est repellendus commodi culpa, consectetur voluptas reprehenderit perferendis voluptate quia molestiae exercitationem, perspiciatis quis illum?"
-            title="title2"
-            :isPubllished="false"/>
-            <LessonPreview
 
-            desc="
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur error possimus exercitationem deleniti maiores. Asperiores reiciendis pariatur rerum, quis officiis repellat repellendus iusto, veniam dolore libero, consectetur corporis vero doloribus! Deserunt nesciunt odio voluptatum animi est repellendus commodi culpa, consectetur voluptas reprehenderit perferendis voluptate quia molestiae exercitationem, perspiciatis quis illum?"
-            title="title2"
-            :isPubllished="false"/>
+            <LessonPreview v-for=" elem in data" :post="elem"/>
+
 
         </div>
         <Pagination/>
