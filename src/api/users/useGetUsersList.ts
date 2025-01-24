44618e8284d78947a6c5e0d8b1c24dfbdcc3f5a4
FAST <krinickij.v.a@gmail.com>
2025-01-24 13:35:14 +0300

    REFACTOR: getting UserList API

--- src/api/users/useGetUsersList.ts
+++ src/api/users/useGetUsersList.ts
@@ -1,36 +1,9 @@
-import { SiteState } from "../../store/SiteState"
-
-const url = import.meta.env.VITE_API_DB_URL
+import { apiFetch } from "../../utils/api/apiFetch"
 
 export default function () {
-    const siteState = SiteState()
-
-    const getUserList = async () => {
-        siteState.loadingTrue()
-        siteState.cleanTextError()
-
-        return fetch(url + '/admin_profile', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then(async (res) => {
-            if (!res.ok) {
-                const errorMessage = await res.text
-                const errorText = `HTTP error! status: ${res.status}, message: ${errorMessage}`
-                siteState.errorText = errorText
-                siteState.loadingFalse()
-                throw new Error(errorText);
-            }
-            const data = res.json()
-            return data;
-        })
-            .catch((err) => {
-                siteState.errorText = err.text
-            throw err
-        })
-            .finally(() => {
-                siteState.loadingFalse()
+    const getUserList = async () =>{
+        return apiFetch('/admin_profile',{
+            method:'GET'
         })
     }
     return {
