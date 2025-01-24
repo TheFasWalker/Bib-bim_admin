59d8a70ce6f9f1cfc783682244da08a2f40ba447
FAST <krinickij.v.a@gmail.com>
2025-01-24 13:25:45 +0300

    REFACTOR :deleting user

--- src/api/users/useDeleteUser.ts
+++ src/api/users/useDeleteUser.ts
@@ -1,50 +1,17 @@
-import { SiteState } from "../../store/SiteState"
-import { UserSate } from "../../store/UserState"
 import { UserListState } from "../../store/UsersListState"
-
-const url = import.meta.env.VITE_API_DB_URL
+import { apiFetch } from "../../utils/api/apiFetch"
 
 export default function () {
-    const siteState = SiteState()
-    const userState = UserSate()
+   
     const userListState = UserListState()
+    
     const deleteUserById = async (id:string) => {
-        siteState.loadingTrue()
-        if(id == userState.getUserId){
-            siteState.errorText = 'Самоудаление не рекомендовано'
-            return
-        }
-        
-
-        const headersData = {
-            'Content-Type': 'application/json',
-        }
-        if (userState.getUserToken) {
-            headersData['Authorization'] = `Bearer ${userState.userToken}`
-        }
-
-        return fetch(url + `/admin_profile?id=${id}`, {
-            method: 'DELETE',
-            headers: headersData,
-        })
-        .then(async (res) => {
-            if (!res.ok) {
-                const errorMessage = await res.text
-                const errorText = `HTTP error! status: ${res.status}, message: ${errorMessage}`
-                siteState.errorText = errorText
-                throw new Error(errorText);
-            }
+        return apiFetch(`/admin_profile?id=${id}`,{
+            method:"DELETE"
+        }).then((data)=>{
             userListState.deleteUserById(id)
-            return res.text;
+            return data.text;
         })
-        .catch((err) => {
-            siteState.errorText = 'Неудалось удалить пользователя';
-            throw err;
-          })
-          .finally(() => {
-            siteState.loadingFalse()
-          });
-
     }
     return {
         deleteUserById
