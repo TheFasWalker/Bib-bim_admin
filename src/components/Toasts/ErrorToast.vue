df346f07ec13cd80d09cf6a23d1c93f9d7a8d4a7
FAST <krinickij.v.a@gmail.com>
2025-02-02 07:31:02 +0300

    FIX: fix styles ang logic for ErrorToast

--- src/components/Toasts/ErrorToast.vue
+++ src/components/Toasts/ErrorToast.vue
@@ -4,18 +4,18 @@ import { SiteState } from '../../store/SiteState';
 const showToastLength = import.meta.env.VITE_SHOW_TOAST_TIME
 
 const store = SiteState()
-watch(() => store.sucsesMessage, (newMessage) => {
+watch(() => store.errorText, (newMessage) => {
   if (newMessage) {
     setTimeout(() => {
-      store.cleanSucsessText();
+      store.cleanTextError();
     }, showToastLength*1000);
   }
 });
 </script>
 <template>
 
-<Transition >
-<div v-if="store.errorText"  class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow absolute right-0 bottom-5 z-50" >
+<Transition name="slide-fade">
+<div v-if="store.errorText"  class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow fixed right-0 bottom-5 z-50" >
     <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg ">
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
@@ -38,3 +38,18 @@ watch(() => store.sucsesMessage, (newMessage) => {
 
 
 </template>
+<style lang="css" scoped >
+.slide-fade-enter-active {
+  transition: all .5s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all .5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(100%);
+  opacity: 0;
+}
+</style>
